937f00ca1f65eed6a78ee84b47fa2cc58c8576c9
hiendt-1730 <dong.thi.hien@sun-asterisk.com>
2020-09-14 10:16:16 +0700

    Config Firestore

--- app/src/main/java/com/framgia/kotlintraining/moviedb/screen/firestore/FirestoreFragment.kt
+++ app/src/main/java/com/framgia/kotlintraining/moviedb/screen/firestore/FirestoreFragment.kt
@@ -1,12 +1,20 @@
 package com.framgia.kotlintraining.moviedb.screen.firestore
 
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.View
 import com.framgia.kotlintraining.moviedb.R
 import com.framgia.kotlintraining.moviedb.base.BaseFragment
 import com.framgia.kotlintraining.moviedb.databinding.FragmentFirestoreBinding
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_firestore.*
 
-class FirestoreFragment : BaseFragment<FragmentFirestoreBinding, FirestoreViewModel>(){
+class FirestoreFragment : BaseFragment<FragmentFirestoreBinding, FirestoreViewModel>() {
+
+    private val KEY_NAME = "KEY_NAME"
+    private val KEY_ROOM = "KEY_ROOM"
 
     companion object {
         fun newInstance() = FirestoreFragment()
@@ -17,9 +25,65 @@ class FirestoreFragment : BaseFragment<FragmentFirestoreBinding, FirestoreViewMo
     override val layoutRes: Int
         get() = R.layout.fragment_firestore
 
-    val fireStore = Firebase.firestore
-
     override fun initComponent(viewBinding: FragmentFirestoreBinding) {
+        setEvent()
+    }
 
+    private val documentRef = FirebaseFirestore.getInstance().document("sampleData/home")
+
+    private fun setEvent() {
+        btnSend.setOnClickListener {
+            saveData()
+        }
+        btnFetch.setOnClickListener {
+            fetchData()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        documentRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.d("-------->", "FirestoreFragment - Listen fail!")
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                val name = snapshot.getString(KEY_NAME)
+                val room = snapshot.getString(KEY_ROOM)
+                textFetch.text = "$name-----$room"
+            } else {
+                Log.d("-------->", "FirestoreFragment - Data null!")
+            }
+        }
+    }
+
+    private fun saveData() {
+        val dataToSave = HashMap<String, Any>()
+        dataToSave[KEY_NAME] = editTextName.text.toString()
+        dataToSave[KEY_ROOM] = editTextRoom.text.toString()
+
+        documentRef.set(dataToSave)
+            .addOnSuccessListener {
+                Log.d("-------->", "FirestoreFragment - initComponent: Add data success!")
+            }
+            .addOnFailureListener {
+                Log.d("-------->", "FirestoreFragment - initComponent: Add data failure!")
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun fetchData() {
+        documentRef.get()
+            .addOnSuccessListener{ documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val name = documentSnapshot.getString(KEY_NAME)
+                    val room = documentSnapshot.getString(KEY_ROOM)
+                    textFetch.text = "$name-----$room"
+                }
+            }
+            .addOnFailureListener{
+                Log.d("-------->", "FirestoreFragment - fetchData: Fail!")
+            }
     }
 }
